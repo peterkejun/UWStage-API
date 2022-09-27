788a62a6c39046072025e539f25685225b0d18d1
Peter Ke <j5ke@uwaterloo.ca>
2022-09-27 18:06:39 -0400

    simple endpoints for /company

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,18 +1,40 @@
-import { Controller } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { getHttpException } from 'src/utils/http-response';
+import { UpdateResult } from 'typeorm';
 import { Company } from './company.entity';
 import { CompanyService } from './company.service';
 
 @Controller('company')
 export class CompanyController {
-  constructor(
-    private readonly companyService: CompanyService,
-    private dataSource: DataSource,
-  ) {}
+  constructor(private readonly companyService: CompanyService) {}
 
-  async createOne(company: Company) {
-    await this.dataSource.transaction(async (manager) => {
-      await manager.save(company);
-    });
+  @Get()
+  public findAll(): Promise<Company[]> {
+    return this.companyService.findAll()
   }
+
+  @Get(':id')
+  public async findOne(@Param('id', ParseIntPipe) id: number): Promise<Company> {
+    const company = await this.companyService.findOne(id);
+    if (company == null) {
+      throw getHttpException(HttpStatus.NOT_FOUND);
+    }
+    return company;
+  }
+
+  @Post()
+  public createOne(@Body() company: Company): Promise<Company> {
+    return this.companyService.createOne(company);
+  }
+
+  @Delete(':id')
+  public deleteOne(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.companyService.remove(id)
+  }
+
+  @Post(':id')
+  public updateOne(@Param('id', ParseIntPipe) id: number, @Body() company: Company): Promise<UpdateResult> {
+    return this.companyService.updateOne(id, company);
+  }
+
 }
